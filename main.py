db1dcef920cbe21255cb3956148b715526658b2b
DHRUV SHARMA <dhruvsharma1200@gmail.com>
2026-02-01 11:05:14 -0500

    Program entry

---
+++ main.py
@@ -0,0 +1,36 @@
+#Course CST8002 - Programming Language Research Project
+#Professor : Stanley Pieda
+#Due Date : 1st Feb 2026
+#Author : Dhruv Sharma 
+# File name: dataset.py
+# This file program entry 
+
+from dataset import load_dataset
+
+# Constants
+FULL_NAME = "DHRUV SHARMA"
+CSV_FILE = "pacific_rim_npr_coastalmarine_migratory_shorebird_habitat_use_2011-2017_data.csv"
+
+def main():
+    print("=" * 80)
+    print(f"Student Name: {FULL_NAME}")
+    print("=" * 80)
+
+    # Load dataset 
+    records = load_dataset(CSV_FILE)
+
+    # Loop through records and display from
+    if records:
+        print("\nDataset Records: \n")
+        for record in records:
+            print(record)
+    else:
+        print("No records loaded....")
+    
+    print("\n" + "=" * 80)
+    print(f"Student Name: {FULL_NAME}")
+    print("="* 80)
+
+    #start program 
+if __name__ == "__main__":
+        main()
