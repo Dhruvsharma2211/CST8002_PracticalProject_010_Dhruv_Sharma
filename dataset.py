c2e8b0f6d5d892e1b4693c85d251ead4609247c8
DHRUV SHARMA <dhruvsharma1200@gmail.com>
2026-02-01 11:06:11 -0500

    handle file input and output and loading data

---
+++ dataset.py
@@ -0,0 +1,37 @@
+#Course CST8002 - Programming Language Research Project
+#Professor : Stanley Pieda
+#Due Date : 1st Feb 2026
+#Author : Dhruv Sharma 
+# File name: dataset.py
+# This file handles File input and output and loading data from CSV file
+
+import csv # library usage
+from record import Record # import record class
+
+def load_dataset(file_path, max_records=5):
+    records = [] #list to store record object
+    try: 
+        # opening CSV file 
+        with open(file_path, newline='', encoding="latin1") as csv_file:
+            reader = csv.DictReader(csv_file)
+
+            # Read first few records
+            for index, row in enumerate(reader):
+                if index >= max_records:
+                    break
+                #create record object using CSV column names
+                record = Record(
+                    row["Site identification"],
+                    row["Area"],
+                    row["Visit date"],
+                    row["Start time"],
+                    row["Species code"],
+                    row["Count"]
+                )
+
+                records.append(record)
+    except FileNotFoundError:
+        print(" Error :OPPPSSSSSS CSV file not found...")
+    except Exception as error:
+        print(f"An error occurred:{error}")
+    return records
