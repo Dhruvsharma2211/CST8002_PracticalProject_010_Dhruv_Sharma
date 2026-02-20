0c533ba8987b1f33f31f0a71e5fa660731dc07e5
DHRUV SHARMA <dhruvsharma1200@gmail.com>
2026-02-20 18:28:52 -0500

    Correcting Imports in FIle

--- practical_project_2/persistence/csv_repository.py
+++ practical_project_2/persistence/csv_repository.py
@@ -9,7 +9,7 @@ import uuid
 from pathlib import Path
 from typing import List
 
-from model.record import Record, FIELDS
+from practical_project_2.model.record import Record, FIELDS
 
 class CsvRepository:
     def __init__(self, dataset_path: str) -> None:
