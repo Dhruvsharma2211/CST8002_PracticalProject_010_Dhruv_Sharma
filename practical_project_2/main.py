4e22f5ba06db75e7cd62e7708db26dbcaa97ffe5
DHRUV SHARMA <dhruvsharma1200@gmail.com>
2026-02-20 19:21:59 -0500

    Updating code

--- practical_project_2/main.py
+++ practical_project_2/main.py
@@ -1,17 +1,18 @@
 # Author : Dhruv Sharma
 #File Name: main.py
 
-from practical_project_2.controller.record_controller import StudentController
-from practical_project_2.business.record_service import StudentService
-from persistence.csv_repository import CsvRepository
-from presentation.cli_view import CliView
+from practical_project_2.controller.record_controller import RecordController
+from practical_project_2.business.record_service import RecordService
+from practical_project_2.persistence.csv_repository import CsvRepository
+from practical_project_2.presentation.cli_view import CliView
 
 def main() -> None:
-    dataset_path = "dataset/pacific_rim_npr_coastalmarine_migratory_shorebird_habitat_use_2011-2017_data.csv"
+    dataset_path = "practical_project_2/" \
+    "dataset/pacific_rim_npr_coastalmarine_migratory_shorebird_habitat_use_2011-2017_data.csv"
 
     repo = CsvRepository(dataset_path=dataset_path)
-    service = StudentService(repo=repo)
-    controller = StudentController(service=service)
+    service = RecordService(repo=repo)
+    controller = RecordController(service=service)
     view = CliView(controller=controller, full_name="Dhruv Sharma")
 
     view.run()
