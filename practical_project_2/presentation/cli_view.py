6d55d3d879462cb4559b371d6c7749348b2b345b
DHRUV SHARMA <dhruvsharma1200@gmail.com>
2026-02-22 05:25:26 -0500

    Final Update and Add References

--- practical_project_2/presentation/cli_view.py
+++ practical_project_2/presentation/cli_view.py
@@ -7,7 +7,6 @@ Reference:
 [1] 	D. Hillard, "Effective Python Testing With pytest," realpython.com, 08 Dev 2024. [Online]. Available: https://realpython.com/pytest-python-testing/. [Accessed 22 02 2026].
 [2] 	R. Oliveira, "GUID vs UUID vs ULID: Understanding Unique Identifiers," medium.com, 31 Jul 2024. [Online]. Available: https://medium.com/@ronaldo.oliver7/guid-vs-uuid-vs-ulid-understanding-unique-identifiers-565c88cdca13. [Accessed 22 Feb 2026].
 [3] 	C. Team, "MVC Architecture Explained: Model, View, Controller," codecademy.com, N.D.. [Online]. Available: https://www.codecademy.com/article/mvc-architecture-model-view-controller. [Accessed 22 Feb 2026].
-
 """
 
 from typing import Optional
