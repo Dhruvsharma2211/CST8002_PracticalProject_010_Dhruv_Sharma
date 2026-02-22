1137562922e56b4414cb38e16874ea3e2c7463e0
DHRUV SHARMA <dhruvsharma1200@gmail.com>
2026-02-22 05:23:03 -0500

    Final Update and Add References

--- practical_project_2/business/record_service.py
+++ practical_project_2/business/record_service.py
@@ -7,7 +7,6 @@ Reference:
 [2] 	R. Oliveira, "GUID vs UUID vs ULID: Understanding Unique Identifiers," medium.com, 31 Jul 2024. [Online]. Available: https://medium.com/@ronaldo.oliver7/guid-vs-uuid-vs-ulid-understanding-unique-identifiers-565c88cdca13. [Accessed 22 Feb 2026].
 [3] 	C. Team, "MVC Architecture Explained: Model, View, Controller," codecademy.com, N.D.. [Online]. Available: https://www.codecademy.com/article/mvc-architecture-model-view-controller. [Accessed 22 Feb 2026].
 
-
 """
 
 from typing import List, Optional
